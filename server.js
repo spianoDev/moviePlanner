35b534f8184c62eb24a09831a833a3b703e4ef06
spiano <lafleurpiano@gmail.com>
2020-07-30 20:51:45 -0500

    framework and file system created along with basic server function

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mysql = require("mysql");
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "movie_planner_db"
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+
+    console.log("connected as id " + connection.threadId);
+});
+
+//Routes below
+
+app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT}`));
